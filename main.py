9379fc47e13e297ec1cca240c3efedb7d37e3018
cyberhades21 <79735414+cyberhades21@users.noreply.github.com>
2023-01-16 10:38:43 +0530

    importing packages

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+from flask import Flask,render_template,request,redirect,url_for
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
+import numpy as np
+import six.moves.urllib as urlib
+ 
