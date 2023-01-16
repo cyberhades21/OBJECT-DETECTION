81cbf4654bd927588a02f8fb3748241b8691c33f
cyberhades21 <79735414+cyberhades21@users.noreply.github.com>
2023-01-16 10:45:36 +0530

    downloaded reqired files

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
 import numpy as np
 import six.moves.urllib as urlib
+import sys
+import tensorflow as tf
+from collections import defaultdict
+from io import StringIO
+from PIL import Image
+sys.path.append("..")
  
